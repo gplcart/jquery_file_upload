5c3acaf0127df461141b8f477419eec73c70a58a
GplCart <gplcart.software@gmail.com>
2017-10-12 01:52:58 +0300

    Add code base

---
+++ JqueryFileUpload.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @package Jquery File Upload
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2017, Iurii Makukh <gplcart.software@gmail.com>
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPL-3.0+
+ */
+
+namespace gplcart\modules\jquery_file_upload;
+
+use gplcart\core\Module;
+
+/**
+ * Main class for Jquery File Upload module
+ */
+class JqueryFileUpload extends Module
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Implements hook "library.list"
+     * @param array $libraries
+     */
+    public function hookLibraryList(array &$libraries)
+    {
+        $libraries['jquery_file_upload'] = array(
+            'name' => /* @text */'jQuery File Upload',
+            'description' => /* @text */'File Upload widget with multiple file selection, drag&drop support, progress bars, validation and preview images, audio and video for jQuery',
+            'type' => 'asset',
+            'module' => 'file_manager',
+            'url' => 'https://github.com/blueimp/jQuery-File-Upload',
+            'download' => 'https://github.com/blueimp/jQuery-File-Upload/archive/v9.19.1.zip',
+            'version_source' => array(
+                'file' => 'vendor/jquery-file-upload/package.json'
+            ),
+            'files' => array(
+                'vendor/jquery-file-upload/js/jquery.fileupload.js'
+            ),
+        );
+    }
+
+    /**
+     * Implements hook "module.enable.after"
+     */
+    public function hookModuleEnableAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.disable.after"
+     */
+    public function hookModuleDisableAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.install.after"
+     */
+    public function hookModuleInstallAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.uninstall.after"
+     */
+    public function hookModuleUninstallAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://scrutinizer-ci.com/g/gplcart/jquery_file_upload/badges/build.png?b=master)](https://scrutinizer-ci.com/g/gplcart/jquery_file_upload/build-status/master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/gplcart/jquery_file_upload/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/gplcart/jquery_file_upload/?branch=master)
+
+Jquery File Upload is a [GPL Cart](https://github.com/gplcart/gplcart) module that adds [Jquery File Upload](https://github.com/blueimp/jQuery-File-Upload) library for other modules.
+
+**Installation**
+
+1. Download and extract to `system/modules` manually or using composer `composer require gplcart/jquery_file_upload`. IMPORTANT: If you downloaded the module manually, be sure that the name of extracted module folder doesn't contain a branch/version suffix, e.g `-master`. Rename if needed.
+2. Go to `admin/module/list` end enable the module
