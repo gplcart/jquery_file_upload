a152d894d10504fa7de365f301f7ed3037dc5530
GplCart <gplcart.software@gmail.com>
2017-11-06 17:48:30 +0200

    Core API changes

--- JqueryFileUpload.php
+++ JqueryFileUpload.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\jquery_file_upload;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Jquery File Upload module
@@ -18,11 +19,11 @@ class JqueryFileUpload extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
